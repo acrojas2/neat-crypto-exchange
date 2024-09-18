bebf74304cbe53fc8e63400c9df799e285039ec2
Alejandra Rojas <acrojaslopez@gmail.com>
2024-09-18 13:41:57 -0300

    feat(components): add wallet components

---
+++ src/app/components/wallet-card/wallet-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'wallet-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './wallet-card.component.html',
+  styleUrl: './wallet-card.component.scss'
+})
+export class WalletCardComponent {
+  @Input() balance: number | undefined;
+  @Input() name: string | undefined;
+  @Input() code: string | undefined;
+}
+
+
+
+
+
