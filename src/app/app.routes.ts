3f49207ccbf9442c5b43c55487d39bd0d23d8bb6
Alejandra Rojas <acrojaslopez@gmail.com>
2024-09-21 20:34:06 -0300

    feat: add new route for buy-sell form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthRedirectGuard } from './guards/auth-redirect.guard';
+import { BuySellFormComponent } from './components/buy-sell-form/buy-sell-form.component';
 
 export const routes: Routes = [
   { path: 'sign-up', component: SignupComponent, canActivate: [AuthRedirectGuard] },
   { path: 'login', component: LoginComponent, canActivate: [AuthRedirectGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'buy-sell', component: BuySellFormComponent, canActivate: [AuthGuard] },
 ];
