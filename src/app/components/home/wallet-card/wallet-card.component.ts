99a9dcfd07efcfb54ca3382275ceb4b0743da28a
Alejandra Rojas <acrojaslopez@gmail.com>
2024-09-22 19:21:23 -0300

    feat(wallet): show frozen balances

--- src/app/components/home/wallet-card/wallet-card.component.ts
+++ src/app/components/home/wallet-card/wallet-card.component.ts
@@ -1,17 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'wallet-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './wallet-card.component.html',
   styleUrl: './wallet-card.component.scss'
 })
 export class WalletCardComponent {
   @Input() balance: number | undefined;
-  @Input() frozenBalance: number | undefined;
+  @Input() frozenBalanceToRemove: number | undefined;
+  @Input() frozenBalanceToAdd: number | undefined;
   @Input() name: string | undefined;
   @Input() code: string | undefined;
+
+  allowedToSell(code: string): Boolean {
+    return code !== undefined && code !== 'USD';
+  }
 }
 
 
