9cb6ea823bce69d2b2e4c16b0228017f5e170716
Alejandra Rojas <acrojaslopez@gmail.com>
2024-09-23 01:59:44 -0300

    chore: add firebase config skeleton

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    "projectId": null,
+    "appId": null,
+    "storageBucket": null,
+    "apiKey": null,
+    "authDomain": null,
+    "messagingSenderId": null,
+    "measurementId": null
+  }
+};
